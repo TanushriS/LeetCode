7ef8c5782365a5555fa2d3150b8dc418672f69aa
Tanushri Sukhwal <139938767+TanushriS@users.noreply.github.com>
2025-08-02 01:42:25 +0530

    Time: 128 ms (7.29%) | Memory: 46 MB (10.99%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,24 +1,18 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s.isEmpty()) {
-        	return true;
+        String newString= "";
+        for (int i= 0; i<s.length() ; i++){
+            if(Character.isLetterOrDigit(s.charAt(i))){
+                newString = newString+ s.charAt(i);
+            }
         }
-        int start = 0;
-        int last = s.length() - 1;
-        while(start <= last) {
-        	char currFirst = s.charAt(start);
-        	char currLast = s.charAt(last);
-        	if (!Character.isLetterOrDigit(currFirst )) {
-        		start++;
-        	} else if(!Character.isLetterOrDigit(currLast)) {
-        		last--;
-        	} else {
-        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
-        			return false;
-        		}
-        		start++;
-        		last--;
-        	}
+        newString= newString.toLowerCase();
+        int last = newString.length() -1 ;
+        for (int i =0; i<last; i++){
+            if(newString.charAt(i)!= newString.charAt(last)){
+                return false;
+            }
+            last -= 1;
         }
         return true;
     }
